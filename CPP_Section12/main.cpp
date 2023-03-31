4b2cd1af92694be4cf42a46636beef96c18434ac
A-Cobble <alex.cobble42@gmail.com>
2023-03-31 16:07:21 -0700

    More practice on how to use pointers and references.

--- CPP_Section12/main.cpp
+++ CPP_Section12/main.cpp
@@ -20,7 +20,28 @@ int main() {
 	cout << "Address of p is: " << &p << endl;
 	cout << "sizeof of p is: " << sizeof p << endl;
 
+	int* p1{ nullptr };
+	double* p2{ nullptr };
+	unsigned long long* p3{ nullptr };
+	vector<string>* p4{ nullptr };
+	string* p5{ nullptr };
 
+	cout << "\nsizeof p1 is: " << sizeof p1 << endl;
+	cout << "\nsizeof p2 is: " << sizeof p2 << endl;
+	cout << "\nsizeof p3 is: " << sizeof p3 << endl;
+	cout << "\nsizeof p4 is: " << sizeof p4 << endl;
+	cout << "\nsizeof p5 is: " << sizeof p5 << endl;
+
+	int score{ 10 };
+	double high_temp{ 100.7 };
+	int* score_ptr{ nullptr };
+
+	score_ptr = &score;
+	cout << "Value of score is: " << score << endl;
+	cout << "Address of score is: " << &score << endl;
+	cout << "Value of score_ptr is: " << score_ptr << endl;
+
+	//score_ptr = &high_temp;  //Compiler error because we defined score_ptr to an integer
 
 	cout << endl;
 	return 0;
