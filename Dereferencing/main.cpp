50a954076ac14b34339df158b69abb4ce50cdb8e
A-Cobble <alex.cobble42@gmail.com>
2023-04-03 14:48:00 -0700

    Practiced dereferencing pointers and using pointers with a vector

--- Dereferencing/main.cpp
+++ Dereferencing/main.cpp
@@ -22,6 +22,37 @@ int main() {
 
 	cout << "\n------------------------------" << endl;
 
+	double high_temp{ 100.7 };
+	double low_temp{ 37.4 };
+	double* temp_ptr{ &high_temp };
+
+	cout << *temp_ptr << endl; // 100.7
+	temp_ptr = &low_temp;
+	cout << *temp_ptr << endl; // 37.4
+
+	cout << "\n------------------------------" << endl;
+
+	string name{ "Frank" };
+	string* string_ptr{ &name };
+
+	cout << *string_ptr << endl; // Frank
+	name = "James";
+	cout << *string_ptr << endl; // James
+
+	cout << "\n------------------------------" << endl;
+
+	vector<string> stooges{ "Larry", "Moe", "Curly" };
+	vector<string>* vector_ptr{ nullptr };
+
+	vector_ptr = &stooges;
+
+	cout << "First stooge: " << (*vector_ptr).at(0) << endl; // Larry
+
+	cout << "Stooges: ";
+	for (auto stooge : *vector_ptr) {
+		cout << stooge << " ";
+	}
+
 
 	cout << endl;
 	return 0;
