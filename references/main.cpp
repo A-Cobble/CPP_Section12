529361fe33012655308ab17b2648c9b12acb86e5
A-Cobble <alex.cobble42@gmail.com>
2023-04-24 12:56:22 -0700

    practiced references and looked into what they are actually pointing to

---
+++ references/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+
+	int num{ 100 };
+	int& ref{ num };
+	
+	cout << num << endl;
+	cout << ref << endl;
+
+	num = 200;
+	cout << "\n--------------------------------------------" << endl;
+	cout << num << endl;
+	cout << ref << endl;
+
+	ref = 300;
+	cout << "\n--------------------------------------------" << endl;
+	cout << num << endl;
+	cout << ref << endl;
+	
+	cout << "\n--------------------------------------------" << endl;
+	cout << "--------------------------------------------" << endl;
+
+	vector<string> stooges{ "Larry", "Moe", "Curly" };
+
+	for (auto str : stooges) {
+		str = "Funny";				//str is a COPY of each vector element
+	}
+
+	for (auto str : stooges) {
+		cout << str << endl;
+	}
+
+	cout << "\n--------------------------------------------" << endl;
+	for (auto& str : stooges) {			//str is a reference to each vector element
+		str = "Funny";
+	}
+
+	for (auto const& str : stooges) {	//using const
+		cout << str << endl;			//now the vector elements have changed
+	}
+	cout << endl;
+	return 0;
+}
